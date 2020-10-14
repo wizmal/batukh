d23ab9bc0047ca42297765420f9eaf1d345d29f7
murtaza <themurtazanazir@gmail.com>
2020-10-14 09:24:59 +0530

    Package name change in setup, ready to be uploaded to pypi.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Koshur-Recog-murtaza",  # Replace with your own username
+    name="batukh",
     version="0.0.1",
-    author="Murtaza, Wajid",
-    author_email="myemail@example.com",
-    description="A small example package",
+    author="Murtaza, Wajid, Naveed",
+    # author_email="myemail@example.com",
+    description="An OCR for multiple languages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/KoshurNizam/batukh",
