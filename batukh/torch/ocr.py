d96feee906db92c4e6a7fff7e79a01a4856aaf67
murtaza <themurtazanazir@gmail.com>
2020-10-18 20:27:04 +0530

    1. Added predict function to torch OCR. 2. Removed batch_size parameter. Batch size cannot be > 1 for OCR.

--- batukh/torch/ocr.py
+++ batukh/torch/ocr.py
@@ -29,7 +29,7 @@ class OCR:
     Args:
         encoder_input (int, optional): Input size of vector that goes into the encoder.
             Default: 1536, based on the assumption that the input shape is
-            `[batch_size, 3, 60, x]`.
+            `[1, 3, 60, x]`.
         encoder_hidden (int, optional): hidden size of the GRU layer of encoder. 
             Default: 128.
         encoder_nlayers (int, optional): number of layers in the GRU layer of encoder.
@@ -184,7 +184,6 @@ class OCR:
               n_epochs,
               train_dl=None,
               val_dl=None,
-              batch_size=1,
               shuffle=True,
               criterion=None,
               imgenc_optimizer=None,
@@ -197,6 +196,44 @@ class OCR:
               max_to_keep=5,
               device=None,
               ):
+        r"""
+        Args:
+            n_epochs (int): number of epochs to train for.
+            train_dl (:class:`~torch.utils.data.DataLoader`, optional): dataloader
+                to use for training. Default None.
+            val_dl (:class:`~torch.utils.data.DataLoader`, optional): dataloader
+                to use for validation. Default None.
+            shuffle (bool, optional): whether to shuffle the default dataloader.
+                Default: True.
+            criterion (:class:`~torch.nn.module.loss._Loss` or :class:`~torch.nn.module.loss._WeightedLoss`, optional): 
+                The loss function to use.
+                Default: ``CrossEntropyLoss(reduction="mean")``
+            imgenc_optimizer (:class:`~torch.optim.Optimizer`, optional): 
+                optimizer for img_encoder.
+                Default: Adam.
+            enc_optimizer (:class:`~torch.optim.Optimizer`, optional): 
+                optimizer for encoder.
+                Default: Adam.
+            dec_optimizer (:class:`~torch.optim.Optimizer`, optional): 
+                optimizer for decoder.
+                Default: Adam.
+            learning_rate (int, optional): learning rate to be used for default
+                optimizers.
+                Default: 0.001.
+            save_checkpoints (bool, optional): whether to save checkpoints or
+                not.
+                Default: True.
+            checkpoint_freq (int, optional): the frequency of saving saving
+                checkpoints.
+                Default: :math:`\left \lfloor\frac{\text{n_epochs}}{10}\right \rfloor + 1`.
+            checkpoint_path (str, optional): Path of the checkpoint folder.
+                Default: "./"
+            max_to_keep (int or None, optional): The maximum number of latest 
+                checkpoints to be saved. ``None`` to save all. 
+                Default: 5.
+            device (str, optional): The device to do computations on.
+                Default: GPU, if GPU is available, else CPU. 
+        """
 
         if device is None:
             device = torch.device(
@@ -206,7 +243,9 @@ class OCR:
         self.encoder.to(device)
         self.decoder.to(device)
 
-        imgenc_optimizer, enc_optimizer, dec_optimizer = self.initOptimizers()
+        imgenc_optimizer, enc_optimizer, dec_optimizer = self.initOptimizers(
+            imgenc_optimizer, enc_optimizer, dec_optimizer, learning_rate
+        )
 
         if criterion is None:
             criterion = nn.CrossEntropyLoss()
@@ -222,12 +261,12 @@ class OCR:
                     raise Exception(
                         "No DataLoader found. Either pass one in train or use load_data method.")
                 else:
-                    train_dl = self.train_dl(batch_size, shuffle)
+                    train_dl = self.train_dl(1, shuffle)
             if val_dl is None:
                 if getattr(self, "val_dl", None) is None:
                     val_dl = None
                 else:
-                    val_dl = self.val_dl(batch_size, shuffle)
+                    val_dl = self.val_dl(1, shuffle)
 
             self.img_encoder.train()
             self.encoder.train()
@@ -250,7 +289,7 @@ class OCR:
                 # maybe add teacher_force_ratio control
                 x = x.to(device)
                 y = y.to(device)
-                y = y[0]  # only for batch_size=1, look into it
+                y = y[0]
 
                 loss, target_len = self.forward_step(
                     x, y, criterion, device)
@@ -296,6 +335,57 @@ class OCR:
                 pbar.close()
             if epoch % checkpoint_freq == 0:
                 self.save_model(checkpoint_path, epoch)
+
+    def predict(self, x, device=None):
+        r"""
+        predicts the output given a tensor.
+
+        Args:
+            x (:class:`~torch.Tensor`): tensor of shape ``[1, 3, width, height]``.
+            device (str, optional): device to predict on.
+                Default: GPU if it is available else CPU.
+
+        Returns:
+            list: sequence of outputs.
+        """
+        self.img_encoder.eval()
+        self.encoder.eval()
+        self.decoder.eval()
+
+        if device is None:
+            device = torch.device(
+                "cuda" if torch.cuda.is_available() else "cpu")
+
+        outputs = []
+
+        out = self.img_encoder(x)
+
+        enc_hidden = self.encoder.initHidden()
+
+        encoder_outputs = torch.zeros(
+            self.max_length, self.encoder.hidden_size, device=device)
+
+        for idx in range(out.shape[2]):
+            enc_out, enc_hidden = self.encoder(out[:, :, idx], enc_hidden)
+            encoder_outputs[idx] = enc_out[0, 0]
+
+        decoder_input = torch.tensor(
+            [[[self.train_dl.SOS]]], device=device)
+
+        dec_hidden = enc_hidden
+
+        while True:
+            dec_out, dec_hidden, dec_attn = self.decoder(
+                decoder_input, dec_hidden, encoder_outputs)
+
+            topv, topi = dec_out.topk(1)
+            decoder_input = topi.squeeze().detach()
+
+            outputs.append(decoder_input)
+            if decoder_input.item() == self.train_dl.EOS:
+                break
+
+        return outputs
 
     def load_model(self, path):
         models = torch.load(path)
